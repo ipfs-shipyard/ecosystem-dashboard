ebaada6fba6edcd96b3a7ea3820294eede809fb8
Andrew Nesbitt <andrewnez@gmail.com>
2020-02-14 18:34:56 +0000

    autonome is not a collaborator!

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -13,7 +13,8 @@ class Issue < ApplicationRecord
                 "jessicaschilling", 'aschmahmann', 'dirkmc', 'ericronne', 'andrew',
                 "Mr0grog", 'rvagg', 'lanzafame', 'mikeal', 'warpfork', 'terichadbourne',
                 'mburns', 'nonsense', 'twittner', 'momack2', 'creationix', 'djdv',
-                'jimpick', 'meiqimichelle', 'mgoelzer', 'kishansagathiya', 'dryajov']
+                'jimpick', 'meiqimichelle', 'mgoelzer', 'kishansagathiya', 'dryajov',
+                'autonome']
 
   scope :protocol, -> { where(org: PROTOCOL_ORGS) }
   scope :not_protocol, -> { where.not(org: PROTOCOL_ORGS) }
