b0e1cbfb39ffd09622c2d4f9d08294bc78c455d9
Andrew Nesbitt <andrewnez@gmail.com>
2020-02-14 11:32:55 +0000

    Filter out more ex-ipfs peeps

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -12,7 +12,7 @@ class Issue < ApplicationRecord
                 "jessicaschilling", 'aschmahmann', 'dirkmc', 'ericronne', 'andrew',
                 "Mr0grog", 'rvagg', 'lanzafame', 'mikeal', 'warpfork', 'terichadbourne',
                 'mburns', 'nonsense', 'twittner', 'momack2', 'creationix', 'djdv',
-                'jimpick', 'meiqimichelle', 'mgoelzer']
+                'jimpick', 'meiqimichelle', 'mgoelzer', 'kishansagathiya', 'dryajov']
 
   scope :protocol, -> { where(org: PROTOCOL_ORGS) }
   scope :not_protocol, -> { where.not(org: PROTOCOL_ORGS) }
