35596e66dc4f1a85235d372d3e3f3f60443833a9
Andrew Nesbitt <andrewnez@gmail.com>
2020-03-13 14:54:44 +0000

    link to org pages from collabs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'collabs', to: 'issues#collabs'
   get 'all', to: 'issues#all'
-  get 'orgs/:id', to: 'orgs#show'
+  get 'orgs/:id', to: 'orgs#show', as: :org
   get 'orgs', to: 'orgs#protocol'
 
 
