2d60ca285fb790e36dca47205e612868de4695ac
Andrew Nesbitt <andrewnez@gmail.com>
2020-03-13 14:53:29 +0000

    Individual org stats pages

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -7,20 +7,36 @@ class OrgsController < ApplicationController
     end
 
     @orgs =Issue::PROTOCOL_ORGS.map do |org|
-      {
-        name: org,
-        issues_count: scope.issues.org(org).count,
-        open_issues_count: scope.issues.org(org).state('open').count,
-        closed_issues_count: scope.issues.org(org).state('closed').count,
-        pull_requests_count: scope.pull_requests.org(org).count,
-        open_pull_requests_count: scope.pull_requests.org(org).state('open').count,
-        closed_pull_requests_count: scope.pull_requests.org(org).state('closed').count,
-        comments: scope.org(org).sum(:comments_count),
-        contributors: scope.org(org).humans.employees.group(:user).count.sort_by(&:last).reverse,
-        community_contributors: scope.org(org).not_employees.group(:user).count.sort_by(&:last).reverse,
-        bots: scope.org(org).bots.group(:user).count.sort_by(&:last).reverse,
-        repos: scope.org(org).group(:repo_full_name).count.sort_by(&:last).reverse
-      }
+      load_org_data(scope, org)
     end
+  end
+
+  def show
+    scope = Issue.all
+
+    if params[:range].present?
+      scope = scope.where('created_at > ?', params[:range].to_i.days.ago)
+    end
+
+    @orgs = [load_org_data(scope, params[:id])]
+  end
+
+  private
+
+  def load_org_data(scope, org)
+    {
+      name: org,
+      issues_count: scope.issues.org(org).count,
+      open_issues_count: scope.issues.org(org).state('open').count,
+      closed_issues_count: scope.issues.org(org).state('closed').count,
+      pull_requests_count: scope.pull_requests.org(org).count,
+      open_pull_requests_count: scope.pull_requests.org(org).state('open').count,
+      closed_pull_requests_count: scope.pull_requests.org(org).state('closed').count,
+      comments: scope.org(org).sum(:comments_count),
+      contributors: scope.org(org).humans.employees.group(:user).count.sort_by(&:last).reverse,
+      community_contributors: scope.org(org).not_employees.group(:user).count.sort_by(&:last).reverse,
+      bots: scope.org(org).bots.group(:user).count.sort_by(&:last).reverse,
+      repos: scope.org(org).group(:repo_full_name).count.sort_by(&:last).reverse
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'collabs', to: 'issues#collabs'
   get 'all', to: 'issues#all'
+  get 'orgs/:id', to: 'orgs#show'
   get 'orgs', to: 'orgs#protocol'
+
 
   root to: 'issues#index'
 end
