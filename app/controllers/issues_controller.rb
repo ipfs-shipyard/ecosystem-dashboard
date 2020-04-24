d5722b2911637b0cdd6ae8bbf57f517532569c54
Andrew Nesbitt <andrewnez@gmail.com>
2020-04-24 11:59:04 +0100

    Only show slow things with no comments

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -38,7 +38,7 @@ class IssuesController < ApplicationController
     @date_range = 9
     @scope = Issue.protocol.not_employees.unlocked.where("html_url <> ''").all_collabs
     @scope = @scope.where('created_at > ?', @date_range.days.ago).where('created_at < ?', 2.days.ago)
-    @slow = @scope.open_for_over_2_days
+    @slow = @scope.open_for_over_2_days.where('comments_count < ?', 1)
     @pagy, @issues = pagy(@slow.order('issues.created_at DESC'))
     @collabs = @slow.all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.sort_by{|k,v| -v }
     @users = @slow.group(:user).count
