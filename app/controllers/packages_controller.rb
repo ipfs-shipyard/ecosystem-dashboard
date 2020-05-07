a7fa5288998fb1ba757b0e8cd5b99636d2e51ab5
Andrew Nesbitt <andrewnez@gmail.com>
2020-05-07 13:50:31 +0100

    Only list protocol packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,6 +1,6 @@
 class PackagesController < ApplicationController
   def index
-    @scope = Package.all
+    @scope = Package.where(repository_id: Repository.protocol.pluck(:id))
     @pagy, @packages = pagy(@scope.order('dependent_repos_count DESC, dependents_count DESC, created_at DESC'))
   end
 
