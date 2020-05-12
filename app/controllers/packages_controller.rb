9983b751badb559fb08b560762f6ce27415515ad
Andrew Nesbitt <andrewnez@gmail.com>
2020-05-12 12:23:44 +0100

    fix package list

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,6 +1,6 @@
 class PackagesController < ApplicationController
   def index
-    @scope = Package.where(repository_id: Repository.protocol.pluck(:id)).includes(:repository, :manifest)
+    @scope = Package.where(repository_id: Repository.protocol.pluck(:id)).includes(:repository)
     @pagy, @packages = pagy(@scope.order('collab_dependent_repos_count DESC, dependent_repos_count DESC, created_at DESC'))
   end
 
