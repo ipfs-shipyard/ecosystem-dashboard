14b8739f685f0000ee01fe3c502e1dff2749f7cf
Andrew Nesbitt <andrewnez@gmail.com>
2020-05-12 11:45:35 +0100

    Sort packages by collab dependent repo count

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,7 +1,7 @@
 class PackagesController < ApplicationController
   def index
     @scope = Package.where(repository_id: Repository.protocol.pluck(:id))
-    @pagy, @packages = pagy(@scope.order('dependent_repos_count DESC, dependents_count DESC, created_at DESC'))
+    @pagy, @packages = pagy(@scope.order('collab_dependent_repos_count DESC, dependent_repos_count DESC, created_at DESC'))
   end
 
   def show
