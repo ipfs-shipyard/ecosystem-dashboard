a33241316fb5efc7a89cee50c53d032ebe426ebb
Andrew Nesbitt <andrewnez@gmail.com>
2020-05-13 15:25:48 +0100

    Fix up go package manager support

--- app/models/package_manager/go.rb
+++ app/models/package_manager/go.rb
@@ -1,9 +1,9 @@
 module PackageManager
   class Go < Base
-    HAS_VERSIONS = false
-    HAS_DEPENDENCIES = false
+    HAS_VERSIONS = true
+    HAS_DEPENDENCIES = true
     BIBLIOTHECARY_SUPPORT = true
-    URL = 'http://go-search.org/'
+    URL = 'https://godoc.org/'
     COLOR = '#375eab'
     KNOWN_HOSTS = [
       'bitbucket.org',
@@ -21,7 +21,7 @@ module PackageManager
 
 
     def self.package_link(package, version = nil)
-      "http://go-search.org/view?id=#{package.name}"
+      "http://#{package.name}"
     end
 
     def self.documentation_url(name, version = nil)
@@ -33,20 +33,50 @@ module PackageManager
     end
 
     def self.package_names
-      get("http://go-search.org/api?action=packages")
+      get_raw("https://index.golang.org/index")
     end
 
     def self.package(name)
-      get("http://go-search.org/api?action=package&id=#{name}")
+      {
+        name: name
+      }
     end
 
     def self.mapping(package)
       {
-        name: package['Package'],
+        name: package[:name],
         description: package['Synopsis'],
-        homepage: package['PackageURL'],
-        repository_url: get_repository_url(package)
+        repository_url: get_repository_url(package[:name])
       }
+    end
+
+    def self.versions(package, _name)
+      txt = get_raw("https://proxy.golang.org/#{package[:name]}/@v/list")
+      versions = txt.split("\n")
+
+      versions.map do |v|
+        {
+          number: v,
+          published_at: get_version(package[:name], v).fetch('Time')
+        }
+      end
+    rescue StandardError
+      []
+    end
+
+    def self.dependencies(name, version, _package)
+      mod_file = get_raw("https://proxy.golang.org/#{name}/@v/#{version}.mod")
+
+      Bibliothecary::Parsers::Go.parse_go_mod(mod_file).map do |dep|
+        {
+          package_name: dep[:name],
+          requirements: dep[:requirement],
+          kind: dep[:type],
+          platform: "Go",
+        }
+      end
+    rescue StandardError
+      []
     end
 
     # https://golang.org/cmd/go/#hdr-Import_path_syntax
@@ -68,8 +98,12 @@ module PackageManager
       []
     end
 
-    def self.get_repository_url(package)
-      request("https://#{package['Package']}").to_hash[:url].to_s
+    def self.get_repository_url(package_name)
+      request("https://#{package_name}").to_hash[:url].to_s
+    end
+
+    def self.get_version(package_name, version)
+      get_json("https://proxy.golang.org/#{package_name}/@v/#{version}.info")
     end
   end
 end
