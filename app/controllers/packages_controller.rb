5f9b9f0ce023ba21c1d6f851154099db784cb53d
Andrew Nesbitt <andrewnez@gmail.com>
2020-05-18 11:25:10 +0100

    outdated page should only show protocol packages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -15,6 +15,6 @@ class PackagesController < ApplicationController
   end
 
   def outdated
-    @packages = Package.where('outdated > 0').where('collab_dependent_repos_count > 0').order('outdated DESC').includes(:repository)
+    @packages = Package.protocol.where('outdated > 0').where('collab_dependent_repos_count > 0').order('outdated DESC').includes(:repository)
   end
 end
