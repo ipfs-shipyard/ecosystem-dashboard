284f984149efbea77f1eff5693c7fae81988141d
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-15 17:02:37 +0100

    contributors have events and issues

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,6 +1,9 @@
 class Contributor < ApplicationRecord
   validates_presence_of :github_username
 
+  has_many :events, foreign_key: :user, primary_key: :github_username
+  has_many :issues, foreign_key: :user, primary_key: :github_username
+
   scope :core, -> { where(core: true) }
   scope :bot,  -> { where(bot: true) }
   scope :core_or_bot, -> { core.or(bot) }
