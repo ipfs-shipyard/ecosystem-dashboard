07497bcf3fcc36b87c854651dabfef02414a9aa1
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-15 14:37:56 +0100

    ensure contributors have a name

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -1,4 +1,6 @@
 class Contributor < ApplicationRecord
+  validates_presence_of :github_username
+
   scope :core, -> { where(core: true) }
   scope :bot,  -> { where(bot: true) }
   scope :core_or_bot, -> { core.or(bot) }
