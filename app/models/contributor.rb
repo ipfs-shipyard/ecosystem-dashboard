9b21c0df1e1bd852fec6e2a50476d7932457dc6f
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-16 10:15:29 +0100

    Suggest orgs for a contributor
    Related to #32

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -7,4 +7,10 @@ class Contributor < ApplicationRecord
   scope :core, -> { where(core: true) }
   scope :bot,  -> { where(bot: true) }
   scope :core_or_bot, -> { core.or(bot) }
+
+  def suggest_orgs
+    events = Issue.github_client.user_public_events(github_username)
+    pushes = events.select{|e| e[:type] == 'PushEvent'}
+    orgs = pushes.map(&:org).compact.map(&:login).uniq
+  end
 end
