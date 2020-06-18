f0ef191558c0d66009025eadc704be3fb8ea5848
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-18 14:27:15 +0100

    Scope homepage to not_core

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
   def index
 
-    @new_issues = Issue.internal.this_week.issues.count
-    @new_issues_last_week = Issue.internal.last_week.issues.count
+    @new_issues = Issue.internal.this_week.not_core.issues.count
+    @new_issues_last_week = Issue.internal.last_week.not_core.issues.count
 
-    @new_prs = Issue.internal.this_week.pull_requests.count
-    @new_prs_last_week = Issue.internal.last_week.pull_requests.count
+    @new_prs = Issue.internal.this_week.not_core.pull_requests.count
+    @new_prs_last_week = Issue.internal.last_week.not_core.pull_requests.count
 
     @releases = Event.internal.this_week.event_type('ReleaseEvent').count
 
@@ -15,8 +15,8 @@ class HomeController < ApplicationController
     @forks = Event.internal.this_week.event_type('ForkEvent').count
     @forks_last_week = Event.internal.last_week.event_type('ForkEvent').count
 
-    @comments = Event.internal.this_week.event_type('IssueCommentEvent').count
-    @comments_last_week = Event.internal.last_week.event_type('IssueCommentEvent').count
+    @comments = Event.internal.this_week.not_core.event_type('IssueCommentEvent').count
+    @comments_last_week = Event.internal.last_week.not_core.event_type('IssueCommentEvent').count
 
     @response_time = (Issue.internal.this_week.not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time)/60/60).round(1)
     @response_time_last_week = (Issue.internal.last_week.not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time)/60/60).round(1)
