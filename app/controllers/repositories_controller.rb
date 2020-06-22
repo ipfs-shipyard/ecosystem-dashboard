9eb3b2e82ae06d09528b57906c680a85bb6aedbc
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-22 11:43:59 +0100

    Collab events list page

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -29,4 +29,18 @@ class RepositoriesController < ApplicationController
     @repository = Repository.find(params[:id])
     @manifests = @repository.manifests.includes(repository_dependencies: {package: :versions}).order('kind DESC')
   end
+
+  def collab_events
+    @scope = Event.includes(:repository).external.where('events.created_at > ?', 1.month.ago).humans
+    @scope = @scope.org(params[:org]) if params[:org].present?
+    @scope = @scope.user(params[:user]) if params[:user].present?
+    @scope = @scope.repo(params[:repo_full_name]) if params[:repo_full_name].present?
+    @scope = @scope.event_type(params[:event_type]) if params[:event_type].present?
+    @pagy, @events = pagy(@scope.order('events.created_at DESC'))
+
+    @orgs = @scope.unscope(where: :org).external.group(:org).count
+    @repos = @scope.unscope(where: :repository_full_name).external.group(:repository_full_name).count
+    @users = @scope.unscope(where: :actor).humans.group(:actor).count
+    @event_types = @scope.unscope(where: :event_type).group(:event_type).count
+  end
 end
