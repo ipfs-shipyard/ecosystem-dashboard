1279f2145df2eb06cb7b8c43770378f0afe072e1
Andrew Nesbitt <andrewnez@gmail.com>
2020-06-24 11:40:08 +0100

    Tooltip explainations on homepage stats
    Fixes #44

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,38 +9,38 @@ class HomeController < ApplicationController
       @issues_scope = @issues_scope.org(params[:org])
     end
 
-    period = (params[:range].presence || 7).to_i
+    @period = (params[:range].presence || 7).to_i
 
-    @new_issues = @issues_scope.this_period(period).not_core.issues.count
-    @new_issues_last_week = @issues_scope.last_period(period).not_core.issues.count
+    @new_issues = @issues_scope.this_period(@period).not_core.issues.count
+    @new_issues_last_week = @issues_scope.last_period(@period).not_core.issues.count
 
-    @new_prs = @issues_scope.this_period(period).not_core.pull_requests.count
-    @new_prs_last_week = @issues_scope.last_period(period).not_core.pull_requests.count
+    @new_prs = @issues_scope.this_period(@period).not_core.pull_requests.count
+    @new_prs_last_week = @issues_scope.last_period(@period).not_core.pull_requests.count
 
-    @merged_prs = @issues_scope.where('issues.merged_at > ?', period.days.ago).not_core.pull_requests.count
-    @merged_prs_last_week = @issues_scope.where('issues.merged_at > ?', (period*2).days.ago).where('issues.merged_at < ?', period.days.ago).not_core.pull_requests.count
+    @merged_prs = @issues_scope.where('issues.merged_at > ?', @period.days.ago).not_core.pull_requests.count
+    @merged_prs_last_week = @issues_scope.where('issues.merged_at > ?', (@period*2).days.ago).where('issues.merged_at < ?', @period.days.ago).not_core.pull_requests.count
 
-    @releases = @event_scope.this_period(period).event_type('ReleaseEvent').count
+    @releases = @event_scope.this_period(@period).event_type('ReleaseEvent').count
 
-    @stars = @event_scope.this_period(period).not_core.event_type('WatchEvent').count
-    @stars_last_week = @event_scope.last_period(period).not_core.event_type('WatchEvent').count
+    @stars = @event_scope.this_period(@period).not_core.event_type('WatchEvent').count
+    @stars_last_week = @event_scope.last_period(@period).not_core.event_type('WatchEvent').count
 
-    @forks = @event_scope.this_period(period).not_core.event_type('ForkEvent').count
-    @forks_last_week = @event_scope.not_core.last_period(period).event_type('ForkEvent').count
+    @forks = @event_scope.this_period(@period).not_core.event_type('ForkEvent').count
+    @forks_last_week = @event_scope.not_core.last_period(@period).event_type('ForkEvent').count
 
-    @comments = @event_scope.this_period(period).not_core.event_type('IssueCommentEvent').count
-    @comments_last_week = @event_scope.last_period(period).not_core.event_type('IssueCommentEvent').count
+    @comments = @event_scope.this_period(@period).not_core.event_type('IssueCommentEvent').count
+    @comments_last_week = @event_scope.last_period(@period).not_core.event_type('IssueCommentEvent').count
 
-    @response_time = (@issues_scope.this_period(period).not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time).to_i/60/60).round(1)
-    @response_time_last_week = (@issues_scope.last_period(period).not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time).to_i/60/60).round(1)
+    @response_time = (@issues_scope.this_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time).to_i/60/60).round(1)
+    @response_time_last_week = (@issues_scope.last_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.where.not(response_time: nil).average(:response_time).to_i/60/60).round(1)
 
-    @slow_responses = @issues_scope.this_period(period).not_core.unlocked.where("html_url <> ''").not_draft.slow_response.count
-    @slow_responses_last_week = @issues_scope.last_period(period).not_core.unlocked.where("html_url <> ''").not_draft.slow_response.count
+    @slow_responses = @issues_scope.this_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.slow_response.count
+    @slow_responses_last_week = @issues_scope.last_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.slow_response.count
 
-    @contributors = @issues_scope.this_period(period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys.length
-    @contributors_last_week = @issues_scope.last_period(period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys.length
+    @contributors = @issues_scope.this_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys.length
+    @contributors_last_week = @issues_scope.last_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys.length
 
-    @first_time_contributors = (@issues_scope.this_period(period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys - @issues_scope.where('issues.created_at < ?', period.days.ago).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys).length
-    @first_time_contributors_last_week = (@issues_scope.last_period(period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys - @issues_scope.where('issues.created_at < ?', (period*2).days.ago).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys).length
+    @first_time_contributors = (@issues_scope.this_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys - @issues_scope.where('issues.created_at < ?', @period.days.ago).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys).length
+    @first_time_contributors_last_week = (@issues_scope.last_period(@period).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys - @issues_scope.where('issues.created_at < ?', (@period*2).days.ago).not_core.unlocked.where("html_url <> ''").not_draft.group(:user).count.keys).length
   end
 end
