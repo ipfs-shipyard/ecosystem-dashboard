2523aeef82f9a15e90f1408f3efc8f2435092a39
Andrew Nesbitt <andrewnez@gmail.com>
2020-07-08 12:28:50 +0100

    Add a search highlights page
    Highlights repos and owners that aren't from collabs or core 
    contributors that have shown up in search recently

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,4 +13,18 @@ class SearchController < ApplicationController
     @kinds = @scope.unscope(where: :kind).group(:kind).count
     @orgs = @scope.unscope(where: :org).group(:org).count
   end
+
+  def highlights
+    @range = (params[:range].presence || 7).to_i
+    @scope = SearchResult.this_period(@range).includes(:search_query)
+
+    @scope = @scope.where(kind: params[:kind]) if params[:kind].present?
+
+    @known_orgs = Organization.all.pluck(:name)
+    @known_contributors = Contributor.all.pluck(:github_username)
+    @known_owners = @known_orgs + @known_contributors
+    @orgs = @scope.group_by(&:org).reject{|k,v| v.length < 2}.reject{|k,v| @known_owners.include?(k) }.sort_by{|k,v| -v.length}
+
+    @kinds = @scope.unscope(where: :kind).group(:kind).count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :repositories
   resources :contributors
 
+  get 'search/highlights', to: 'search#highlights', as: :highlights
   get 'search', to: 'search#index', as: :search
   get 'collabs/contributors', to: 'contributors#collabs', as: :collab_contributors
   get 'collabs/packages', to: 'packages#collabs', as: :collab_packages
