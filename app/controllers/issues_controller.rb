49fcf97e6093234c3d0e1f5b559a4ba1ec3efbdf
Andrew Nesbitt <andrewnez@gmail.com>
2020-07-09 15:58:37 +0100

    Exclude draft prs from issue lists by default
    Fixes #52

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -74,6 +74,8 @@ class IssuesController < ApplicationController
     @scope = @scope.exclude_collab(params[:exclude_collab]) if params[:exclude_collab].present?
     @scope = @scope.exclude_label(params[:exclude_label]) if params[:exclude_label].present?
 
+    @scope = @scope.not_draft unless params[:include_drafts].present?
+
     @scope = @scope.where(comments_count: 0) if params[:uncommented].present?
 
     @scope = @scope.no_milestone if params[:no_milestone].present?
