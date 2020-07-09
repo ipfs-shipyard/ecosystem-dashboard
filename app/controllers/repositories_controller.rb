4816af244a3b3c11827d04a3b031e529823e2b3a
Andrew Nesbitt <andrewnez@gmail.com>
2020-07-09 14:40:04 +0100

    Allow sorting of repositories

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -7,19 +7,22 @@ class RepositoriesController < ApplicationController
     @scope = @scope.fork(params[:fork]) if params[:fork].present?
     @scope = @scope.archived(params[:archived]) if params[:archived].present?
 
+    @sort = params[:sort] || 'pushed_at'
+    @order = params[:order] || 'desc'
+
     respond_to do |format|
       format.html do
-        @pagy, @repositories = pagy(@scope.order('repositories.pushed_at DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
 
         @orgs = @scope.unscope(where: :org).internal.group(:org).count
         @languages = @scope.unscope(where: :language).group(:language).count
       end
       format.rss do
-        @pagy, @repositories = pagy(@scope.order('repositories.id DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
         render 'index', :layout => false
       end
       format.json do
-        @pagy, @repositories = pagy(@scope.order('repositories.id DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
         render json: @repositories
       end
     end
@@ -38,19 +41,22 @@ class RepositoriesController < ApplicationController
     @scope = @scope.fork(params[:fork]) if params[:fork].present?
     @scope = @scope.archived(params[:archived]) if params[:archived].present?
 
+    @sort = params[:sort] || 'pushed_at'
+    @order = params[:order] || 'desc'
+
     respond_to do |format|
       format.html do
-        @pagy, @repositories = pagy(@scope.order('repositories.pushed_at DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
 
         @orgs = @scope.unscope(where: :org).internal.group(:org).count
         @languages = @scope.unscope(where: :language).group(:language).count
       end
       format.rss do
-        @pagy, @repositories = pagy(@scope.order('repositories.id DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
         render 'index', :layout => false
       end
       format.json do
-        @pagy, @repositories = pagy(@scope.order('repositories.id DESC'))
+        @pagy, @repositories = pagy(@scope.order(@sort => @order))
         render json: @repositories
       end
     end
