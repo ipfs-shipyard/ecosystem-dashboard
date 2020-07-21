1aa5d15bc536358abddc478ce284edb9633cf397
Andrew Nesbitt <andrewnez@gmail.com>
2020-07-21 16:52:39 +0100

    Refactor issues controller, fixes slow_response rss feed

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -10,6 +10,37 @@ class IssuesController < ApplicationController
     @scope = Issue.internal.humans.unlocked.this_period(@range).includes(:contributor).where("html_url <> ''")
 
     apply_filters
+
+    respond_to do |format|
+      format.html do
+        @types = {
+          'issues' => @scope.issues.count,
+          'pull_requests' => @scope.pull_requests.count
+        }
+
+        @languages = Issue::LANGUAGES.to_h do |language|
+          [language, @scope.unscope(where: :language).language(language).count]
+        end
+
+        @pagy, @issues = pagy(@scope.order(@sort => @order))
+
+        @users = @scope.group(:user).count
+        @states = @scope.unscope(where: :state).group(:state).count
+        @repos = @scope.unscope(where: :repo_full_name).group(:repo_full_name).count
+        @orgs = @scope.unscope(where: :org).internal.group(:org).count
+        @labels = @scope.unscope(where: :labels).pluck(:labels).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+        @collabs = @scope.unscope(where: :collabs).all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+      end
+      format.rss do
+        @pagy, @issues = pagy(@scope.order(@sort => @order))
+
+        render 'all', :layout => false
+      end
+      format.json do
+        @pagy, @issues = pagy(@scope.order(@sort => @order))
+        render json: @issues
+      end
+    end
   end
 
   def slow_response
@@ -20,34 +51,65 @@ class IssuesController < ApplicationController
     @scope = @orginal_scope.where('issues.created_at > ?', @date_range.days.ago).where('issues.created_at < ?', 2.days.ago)
     apply_filters
 
-    @orginal_scope = @orginal_scope.where(repo_full_name: params[:repo_full_name]) if params[:repo_full_name].present?
-    @orginal_scope = @orginal_scope.org(params[:org]) if params[:org].present?
-
-    name = params[:repo_full_name] || params[:org] || 'All Internal Orgs'
-
-    @response_times = [
-      {
-        name: name,
-        data: @orginal_scope.where.not(response_time: nil).where('issues.created_at > ?', 1.year.ago).group_by_week('issues.created_at').average(:response_time).map do |k,v|
-          if v
-            [k,(v/60/60).round(1)]
-          else
-            [k,nil]
-          end
+    respond_to do |format|
+      format.html do
+        @types = {
+          'issues' => @scope.issues.count,
+          'pull_requests' => @scope.pull_requests.count
+        }
+        @languages = Issue::LANGUAGES.to_h do |language|
+          [language, @scope.unscope(where: :language).language(language).count]
         end
-      }
-    ]
 
-    @slow = @scope.slow_response
+        @users = @scope.group(:user).count
+        @states = @scope.unscope(where: :state).group(:state).count
+        @repos = @scope.unscope(where: :repo_full_name).group(:repo_full_name).count
+        @orgs = @scope.unscope(where: :org).internal.group(:org).count
+        @labels = @scope.unscope(where: :labels).pluck(:labels).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+        @collabs = @scope.unscope(where: :collabs).all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
 
-    sort = params[:sort] || 'issues.created_at'
-    order = params[:order] || 'desc'
+        @orginal_scope = @orginal_scope.where(repo_full_name: params[:repo_full_name]) if params[:repo_full_name].present?
+        @orginal_scope = @orginal_scope.org(params[:org]) if params[:org].present?
 
-    @pagy, @issues = pagy(@slow.order(sort => order))
+        name = params[:repo_full_name] || params[:org] || 'All Internal Orgs'
 
-    @collabs = @slow.all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.sort_by{|k,v| -v }
-    @labels = @slow.unscope(where: :labels).pluck(:labels).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
-    @users = @slow.group(:user).count
+        @response_times = [
+          {
+            name: name,
+            data: @orginal_scope.where.not(response_time: nil).where('issues.created_at > ?', 1.year.ago).group_by_week('issues.created_at').average(:response_time).map do |k,v|
+              if v
+                [k,(v/60/60).round(1)]
+              else
+                [k,nil]
+              end
+            end
+          }
+        ]
+
+        @slow = @scope.slow_response
+
+        @pagy, @issues = pagy(@slow.order(@sort => @order))
+
+        @collabs = @slow.all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }.sort_by{|k,v| -v }
+        @labels = @slow.unscope(where: :labels).pluck(:labels).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
+        @users = @slow.group(:user).count
+
+      end
+      format.rss do
+        @slow = @scope.slow_response
+
+        @pagy, @issues = pagy(@slow.order(@sort => @order))
+
+        render 'all', :layout => false
+      end
+      format.json do
+        @slow = @scope.slow_response
+
+        @pagy, @issues = pagy(@slow.order(@sort => @order))
+        render json: @issues
+      end
+    end
+
   end
 
   private
@@ -78,71 +140,17 @@ class IssuesController < ApplicationController
     @scope = @scope.collab(params[:collab]) if params[:collab].present?
     @scope = @scope.all_collabs if params[:only_collabs].present?
 
+    @scope = @scope.language(params[:language]) if params[:language].present?
 
-    sort = params[:sort] || 'issues.created_at'
-    order = params[:order] || 'desc'
-
-    respond_to do |format|
-      format.html do
-
-        @types = {
-          'issues' => @scope.issues.count,
-          'pull_requests' => @scope.pull_requests.count
-        }
-
-        @languages = Issue::LANGUAGES.to_h do |language|
-          [language, @scope.language(language).count]
-        end
-
-        @scope = @scope.language(params[:language]) if params[:language].present?
-
-        if params[:type].present?
-          if params[:type] == 'issues'
-            @scope = @scope.issues
-          else
-            @scope = @scope.pull_requests
-          end
-        end
-
-        @pagy, @issues = pagy(@scope.order(sort => order))
-
-        @users = @scope.group(:user).count
-        @states = @scope.unscope(where: :state).group(:state).count
-        @repos = @scope.unscope(where: :repo_full_name).group(:repo_full_name).count
-        @orgs = @scope.unscope(where: :org).internal.group(:org).count
-        @labels = @scope.unscope(where: :labels).pluck(:labels).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
-        @collabs = @scope.unscope(where: :collabs).all_collabs.pluck(:collabs).flatten.inject(Hash.new(0)) { |h, e| h[e] += 1 ; h }
-
-      end
-      format.rss do
-        @scope = @scope.language(params[:language]) if params[:language].present?
-
-        if params[:type].present?
-          if params[:type] == 'issues'
-            @scope = @scope.issues
-          else
-            @scope = @scope.pull_requests
-          end
-        end
-
-        @pagy, @issues = pagy(@scope.order(sort => order))
-
-        render 'all', :layout => false
-      end
-      format.json do
-        @scope = @scope.language(params[:language]) if params[:language].present?
-
-        if params[:type].present?
-          if params[:type] == 'issues'
-            @scope = @scope.issues
-          else
-            @scope = @scope.pull_requests
-          end
-        end
-
-        @pagy, @issues = pagy(@scope.order(sort => order))
-        render json: @issues
+    if params[:type].present?
+      if params[:type] == 'issues'
+        @scope = @scope.issues
+      else
+        @scope = @scope.pull_requests
       end
     end
+
+    @sort = params[:sort] || 'issues.created_at'
+    @order = params[:order] || 'desc'
   end
 end
