4b514c1782bb5ec5ee73c69278ce1bcc89e69180
Andrew Nesbitt <andrewnez@gmail.com>
2020-07-28 10:27:17 +0100

    Fix download_internal_dependent_packages

--- app/models/package.rb
+++ app/models/package.rb
@@ -486,7 +486,7 @@ class Package < ApplicationRecord
   def self.download_internal_dependent_packages
     Package.internal.each do |package|
       package.platform_class.dependents(package.name).each do |name|
-        unless Package.platform(platform).find_by_name(name)
+        unless Package.platform(package.platform).find_by_name(name)
           package.platform_class.update(name)
         end
       end
