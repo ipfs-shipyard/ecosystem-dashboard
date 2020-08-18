12ca86fb2667f4babcab89256c1b37e53bd1599b
Andrew Nesbitt <andrewnez@gmail.com>
2020-08-18 17:46:09 +0100

    alt/option+click to include rather than exclude dropdown filters

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,9 +9,13 @@
 document.addEventListener("turbolinks:load", function() {
   $(".dropdown-item").click(function (e) {
     if ($(this).data()["alt"]) {
-      if(e.ctrlKey || e.metaKey || e.shiftKey || e.altKey){
+      if(e.ctrlKey || e.metaKey || e.shiftKey){
         e.preventDefault();
         Turbolinks.visit($(this).data().alt)
+      }
+      if(e.altKey){
+        e.preventDefault();
+        Turbolinks.visit($(this).data().add)
       }
     }
   });
