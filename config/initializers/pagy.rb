ff430e4b3b2a93958cf85468671082b33e43074f
Andrew Nesbitt <andrewnez@gmail.com>
2021-02-09 11:42:46 +0000

    Allow up to 1000 items per page

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -2,5 +2,9 @@ require 'pagy/extras/bootstrap'
 require 'pagy/extras/array'
 require 'pagy/extras/headers'
 require 'pagy/extras/overflow'
+require 'pagy/extras/items'
 
 Pagy::VARS[:overflow] = :last_page
+
+Pagy::VARS[:max_items] = 1000
+Pagy::VARS[:items_param] = :per_page
