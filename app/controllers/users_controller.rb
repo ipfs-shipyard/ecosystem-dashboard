0338590338d70161dc9bdb6f8ebe76638b904fa8
Andrew Nesbitt <andrewnez@gmail.com>
2021-02-11 13:24:05 +0000

    Fix event ordering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,9 @@ class UsersController < ApplicationController
     @username = params[:id]
     @events_scope = Event.user(@username)
 
-    @pagy, @events = pagy(@events_scope)
+    sort = params[:sort] || 'events.created_at'
+    order = params[:order] || 'desc'
+
+    @pagy, @events = pagy(@events_scope.order(sort => order))
   end
 end
