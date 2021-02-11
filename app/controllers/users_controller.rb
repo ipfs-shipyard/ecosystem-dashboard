0f5da0fbdf2c8ee336a52c8f6f3ab0df2866cff6
Andrew Nesbitt <andrewnez@gmail.com>
2021-02-11 17:12:00 +0000

    basic user pmf list for various states

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
 
     @pagy, @events = pagy(@events_scope.order(sort => order))
   end
+
+  def index
+    state_name = params[:tab].presence || 'first'
+
+    @start_date = 1.week.ago.beginning_of_week
+    @end_date = Time.now.beginning_of_week
+    @window = 1
+
+    @data = Rails.cache.fetch(['state', state_name, @start_date, @end_date, @window], expires_in: 1.hours) do
+      Pmf.state(state_name, @start_date, @end_date, @window)
+    end
+
+    @pagy, @users = pagy_array(@data.first[:states].first[1])
+  end
 end
