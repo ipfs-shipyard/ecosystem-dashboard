d28715bb1084583d30ed719926da26206dd9684b
Andrew Nesbitt <andrewnez@gmail.com>
2021-02-22 13:11:07 +0000

    another typo

--- app/models/concerns/dependency_miner.rb
+++ app/models/concerns/dependency_miner.rb
@@ -21,7 +21,7 @@ module DependencyMiner
     last_commit_sha = dependency_events.order('committed_at DESC').first.try(:commit_sha)
 
     # store activities as DependencyActivity records
-    commits = mine.analyse(default_branch, last_commit_sha)
+    commits = miner.analyse(default_branch, last_commit_sha)
 
     # only consider commits with dependency data
     dependency_commits = commits.select{|c| c.data[:dependencies].present? }
