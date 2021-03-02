1d5baf274a9bb0e7660dcf0e6311daead355cb11
Andrew Nesbitt <andrewnez@gmail.com>
2021-03-02 09:50:22 +0000

    Add filter for manifest kind

--- app/controllers/dependency_events_controller.rb
+++ app/controllers/dependency_events_controller.rb
@@ -6,6 +6,7 @@ class DependencyEventsController < ApplicationController
     @scope = @scope.where(action: params[:action_name]) if params[:action_name].present?
     @scope = @scope.where(platform: params[:platform]) if params[:platform].present?
     @scope = @scope.where(package_name: params[:package_name]) if params[:package_name].present?
+    @scope = @scope.where(manifest_kind: params[:manifest_kind]) if params[:manifest_kind].present?
 
     sort = params[:sort] || 'dependency_events.committed_at'
     order = params[:order] || 'desc'
@@ -17,6 +18,7 @@ class DependencyEventsController < ApplicationController
         @action_names = @scope.unscope(where: :action).group(:action).count
         @platforms = @scope.unscope(where: :platform).group(:platform).count
         @package_names = @scope.unscope(where: :package_name).group(:package_name).count
+        @manifest_kinds = @scope.unscope(where: :manifest_kind).group(:manifest_kind).count
       end
       format.rss do
         render 'index', :layout => false
