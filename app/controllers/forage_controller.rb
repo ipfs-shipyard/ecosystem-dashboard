c2c1eb54df744bf5adb95e3060c391894a30d464
Andrew Nesbitt <andrewnez@gmail.com>
2021-04-22 18:25:19 +0100

    increase the number of packages listed in the forage endpoint

--- app/controllers/forage_controller.rb
+++ app/controllers/forage_controller.rb
@@ -1,13 +1,12 @@
 class ForageController < ApplicationController
   def index
-    @internal_or_partner_package_ids = Package.internal.pluck(:id)
-    @dependency_ids = RepositoryDependency.where(repository_id: Repository.internal.pluck(:id)).pluck(:package_id).compact
-    @ids = (@internal_or_partner_package_ids + @dependency_ids).uniq
-    @packages = Package.select('id, platform, name').where(platform: ['Go', 'Npm']).where(id: @ids)
-    @json = @packages.map do |package|
+    @internal_or_partner_packages = Package.internal.pluck(:platform, :name)
+    @dependencies = RepositoryDependency.where(repository_id: Repository.internal.pluck(:id)).pluck(:platform, :package_name).uniq
+    @packages = (@internal_or_partner_packages + @dependencies).map{|p| [p[0].downcase, p[1]]}.uniq.select{|p| ['npm','go'].include? p[0]}
+    @json = @packages.sort.map do |package|
       {
-        manager: package.platform.downcase,
-        name: package.name
+        manager: package[0].downcase,
+        name: package[1]
       }
     end
     render json: @json.to_json
