aadfc04f576ad2ea905a788d13165fafa5a9017a
Andrew Nesbitt <andrewnez@gmail.com>
2022-05-02 11:46:07 +0100

    Handle missing data in graphql response

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -202,7 +202,7 @@ class Issue < ApplicationRecord
           }
         GRAPHQL
         res = org_github_client.post '/graphql', { query: query }.to_json
-        ids += res[:data][:repository][:issueOrPullRequest][:projectsNext][:nodes].map{|n| n[:number]}
+        ids += (res.dig(:data, :repository, :issueOrPullRequest, :projectsNext, :nodes) || []).map{|n| n[:number]}
       end
 
       update_column(:board_ids, ids.uniq)
